f0ed67a08e8cc1a10f3e479a98637c89fc5395f4
Cameron Cardinal <cameriancardinal@gmail.com>
2024-08-24 17:54:40 +0200

    started preparing dashboardand regenerated CRUD but it is mostly a draft for a test

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\BaseCharacter;
+use App\Entity\CharacterEidolons;
+use App\Entity\CharacterKit;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -13,23 +17,7 @@ class DashboardController extends AbstractDashboardController
     #[Route('/fledgless', name: 'fledgless')]
     public function index(): Response
     {
-        return parent::index();
-
-        // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
-        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
-
-        // Option 2. You can make your dashboard redirect to different pages depending on the user
-        //
-        // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirect('...');
-        // }
-
-        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        //
-        // return $this->render('some/path/my-dashboard.html.twig');
+        return $this->render('admin/dashboard/index.html.twig');
     }
 
     public function configureDashboard(): Dashboard
@@ -41,6 +29,15 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        
+        yield MenuItem::section('Characters');
+        yield MenuItem::subMenu('Characters', 'fas fa-folder')->setSubItems([
+            MenuItem::linkToCrud('Character list', 'fas fa-list', BaseCharacter::class),
+            MenuItem::linkToCrud('New character', 'fas fa-plus', BaseCharacter::class)->setAction(Crud::PAGE_NEW),
+            MenuItem::linkToCrud('Kits', 'fas fa-list', CharacterKit::class),
+            MenuItem::linkToCrud('New character', 'fas fa-plus', CharacterKit::class)->setAction(Crud::PAGE_NEW),
+            MenuItem::linkToCrud('Eidolons', 'fas fa-list', CharacterEidolons::class),
+            MenuItem::linkToCrud('New character', 'fas fa-plus', CharacterEidolons::class)->setAction(Crud::PAGE_NEW),
+        ]);
     }
 }
